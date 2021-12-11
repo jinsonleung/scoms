4d2ee21e49f72604e549d8d10e2faa62c922c64b
JinsonLeung <1053438108@qq.com>
2021-12-11 12:18:35 +0800

    multisearch.vue components reset

--- frontend/src/views/multiSearch/index.vue
+++ frontend/src/views/multiSearch/index.vue
@@ -1,181 +1,194 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="searFormBox">
-        <el-form ref="searchFormRef" :rules="searchFormRules" :model="searchForm" label-width="80px">
-          <el-form-item label="质量标准" prop="quantity">
-            <el-input v-model="searchForm.quantity" style="width: 180px"
-                      ref="search_content_input" class="itemInput">
-            </el-input>
-          </el-form-item>
+  <h3>==多条件查询==</h3>
+  <h4>https://www.cnblogs.com/jyk/p/14841929.html</h4>
+  <h4>https://www.cnblogs.com/wuhuacong/p/14035302.html</h4>
+  <!--查询条件区域-->
+  <el-form ref="searchFormRef" :model="searchForm" :rules="rules" label-width="80" :inline="true" class="searchForm">
+    <el-form-item label="快速查询" prop="quickSearchValue">
+      <el-select v-model="searchForm.quickSearchValue" clearable placeholder='请选择' style="width: 120px;"
+                 @change="changeHandle">
+        <el-option
+            v-for="item in searchForm.quickSearchOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        >
+        </el-option>
+      </el-select>
+      <el-input v-model="searchForm.searchValue" :placeholder=searchItemPlaceholder style="width: 180px"
+                ref="search_content_input" class="itemInput">
+      </el-input>
+    </el-form-item>
 
-          <el-form-item label="快速查找" prop="quickSearchItemName">
-            <el-select v-model="searchForm.quickSearchOptionValue" clearable placeholder='请选择' style="width: 120px;"
-                       @change="changeHandle">
-              <el-option
-                  v-for="item in quickSearchOptions"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
-              >
-              </el-option>
-            </el-select>
-            <el-input v-model="searchForm.searchValue" placeholder="请输入" style="width: 180px"
-                      ref="search_content_input" class="itemInput">
-            </el-input>
-          </el-form-item>
-
-
-          <el-form-item label="选择器" prop="region">
-            <el-select v-model="searchForm.region" placeholder="请选择">
-              <el-option key="bbk" label="步步高" value="bbk"></el-option>
-              <el-option key="xtc" label="小天才" value="xtc"></el-option>
-              <el-option key="imoo" label="imoo" value="imoo"></el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="日期时间">
-            <el-col :span="11">
-              <el-form-item prop="date1">
-                <el-date-picker type="date" placeholder="选择日期" v-model="searchForm.date1"
-                                style="width: 100%;"></el-date-picker>
-              </el-form-item>
-            </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-              <el-form-item prop="date2">
-                <el-time-picker placeholder="选择时间" v-model="searchForm.date2" style="width: 100%;">
-                </el-time-picker>
-              </el-form-item>
-            </el-col>
-          </el-form-item>
-          <el-form-item label="选择开关" prop="delivery">
-            <el-switch v-model="searchForm.delivery"></el-switch>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">表单提交</el-button>
-            <el-button @click="onReset">重置表单</el-button>
-          </el-form-item>
-        </el-form>
-      </div>
+    <!--展开更多条件-->
+    <div v-if="expandMore">
+      <el-form-item label="所在城市" prop="cityName">
+        <el-input class="itemInput" v-model="searchForm.cityName"></el-input>
+      </el-form-item>
+      <el-form-item label="是否生效" prop="isActivate">
+        <el-select v-model="searchForm.isActivate" placeholder="请选择">
+          <el-option
+              v-for="item in searchForm.isActivateOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="服务类别" prop="serviceLevel">
+        <el-select v-model="searchForm.serviceLevel" placeholder="请选择">
+          <el-option
+              v-for="item in searchForm.serviceLevelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
     </div>
-  </div>
-</template>
+    <!--收起/展开按钮-->
+    <el-button :icon="expandMore ?'el-icon-arrow-up':'el-icon-arrow-down'" type="text" @click="expandMore =!expandMore">
+      {{ expandMore ? '收起' : '展开' }}
+    </el-button>
 
-<script>
-import {reactive, ref} from "vue";
+    <!--查询按钮-->
+    <el-button-group style="margin-left: 20px">
+      <el-button type="primary" icon="el-icons-reset" round @click="searchHandle">查询</el-button>
+      <el-button type="warning" icon="el-icons-reset" @click="resetHandle">重置</el-button>
+      <el-button type="primary" icon="el-icons-search" round @click="advanceSearchHandle">高级查询</el-button>
+    </el-button-group>
+
+  </el-form>
+</template>
+<script lang="ts">
+import {ref, reactive, getCurrentInstance, onMounted, toRefs} from "vue"
+import vPagination from '@/components/pagination.vue'
+import {Search, Edit, ArrowLeft} from '@element-plus/icons'
 import {ElMessage} from "element-plus";
 
+
 export default {
-  name: "baseform",
+  components: {
+    vPagination,
+    Search,
+    Edit,
+  },
   setup() {
-    const options = [
-      {
-        value: "guangdong",
-        label: "广东省",
-        children: [
-          {
-            value: "guangzhou",
-            label: "广州市",
-            children: [
-              {
-                value: "tianhe",
-                label: "天河区",
-              },
-              {
-                value: "haizhu",
-                label: "海珠区",
-              },
-            ],
-          },
-          {
-            value: "dongguan",
-            label: "东莞市",
-            children: [
-              {
-                value: "changan",
-                label: "长安镇",
-              },
-              {
-                value: "humen",
-                label: "虎门镇",
-              },
-            ],
-          },
-        ],
-      },
-      {
-        value: "hunan",
-        label: "湖南省",
-        children: [
-          {
-            value: "changsha",
-            label: "长沙市",
-            children: [
-              {
-                value: "yuelu",
-                label: "岳麓区",
-              },
-            ],
-          },
-        ],
-      },
-    ];
-    const searchFormRef = ref(null);
+    const searchFormRef = ref(null)
     const searchForm = reactive({
-      name: '',
-      region: '',
-      quantity,
-      quickSearchOptionValue: '',
+      quickSearchValue: '',
       searchValue: '',
+      cityName: '',
+      quickSearchOptions: [
+        {
+          value: 'companyAccount',
+          label: '公司账号',
+        },
+        {
+          value: 'companyName',
+          label: '公司名称',
+        },
+        {
+          value: 'serverLevel',
+          label: '服务类别',
+        },
+        {
+          value: 'isActivate',
+          label: '是否生效',
+        },
+        {
+          value: 'EffectiveDate',
+          label: '有效期',
+        }],
+      isActivate: '',
+      isActivateOptions: [
+        {
+          value: 'yes',
+          label: '是',
+        },
+        {
+          value: 'not',
+          label: '否',
+        }
+      ],
+      serviceLevel: '',
+      serviceLevelOptions: [
+        {
+          value: '1',
+          label: '1',
+        },
+        {
+          value: '2',
+          label: '2',
+        },
+        {
+          value: '3',
+          label: '3',
+        },
+        {
+          value: '4',
+          label: '4',
+        },
+        {
+          value: '5',
+          label: '5',
+        },
+      ],
     })
-    const quickSearchOptions = ref([
-      {
-        value: 'companyAccount',
-        label: '公司账号',
-      },
-      {
-        value: 'companyName',
-        label: '公司名称',
-      },
-      {
-        value: 'serverLevel',
-        label: '服务类别',
-      },
-      {
-        value: 'isActivate',
-        label: '是否生效',
-      },
-      {
-        value: 'EffectiveDate',
-        label: '有效期',
-      }])
-    const searchFormRules = {};
-
+    const rules = {}
+    const expandMore = ref(false)
+    const searchItemPlaceholder = ref('请输入')
+    const proxy = getCurrentInstance()
     const changeHandle = () => {
-      // searchItemPlaceholder.value = '请输入' + searchForm.quickSearchOptionValue
-      // // searchForm.searchValue = ''
-      // console.log(searchForm)
+        searchForm.searchValue = ''
+
+      // searchItemPlaceholder.value = '请输入' + searchForm.quickSearchValue
+
+      console.log('quick search value:',searchForm.quickSearchValue)
     }
-    // 提交
-    const onSubmit = () => {
 
-    };
-    // 重置
-    const onReset = () => {
+    // 表单校验
+    // searchFormRef.value.validate((valid) => {
+    //   if (valid) {
+    //     console.log(form);
+    //     ElMessage.success("提交成功！");
+    //   } else {
+    //     return false;
+    //   }
+    // })
 
-      searchFormRef.value.resetFields()
-    };
+    const searchHandle = () => {
+      // if (!quickSearchOptionValue.value || !searchValue.value) return
+      // console.log('==search.key & searchvalue==', quickSearchOptionValue.value, searchValue.value)
+    }
+    const resetHandle = () => {
+      searchForm.searchValue = ''
+      searchFormRef.value.resetFields() //type断言机制报错，不要紧
+    }
+    const advanceSearchHandle = () => {
+
+    }
+
 
     return {
-      options,
-      searchFormRules,
-      searchFormRef,
-      searchForm,
-      quickSearchOptions,
+      rules,
+      expandMore,
+      searchItemPlaceholder,
       changeHandle,
-      onSubmit,
-      onReset,
+      searchForm,
+      searchFormRef,
+      searchHandle,
+      resetHandle,
+      advanceSearchHandle,
     };
-  },
-};
+  }
+}
 </script>
+
+<style>
+.itemInput {
+  width: 150px;
+}
+</style>
