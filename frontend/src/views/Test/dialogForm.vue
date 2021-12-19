69ed424984091d7bd80f9eb000946c20c95721fb
JinsonLeung <1053438108@qq.com>
2021-12-19 20:59:30 +0800

    update sidebar/index.vue

--- frontend/src/views/Test/dialogForm.vue
+++ frontend/src/views/Test/dialogForm.vue
@@ -5,23 +5,6 @@
   <!--  <div v-dialog-drag>-->
   <div v-dialogdrag>
 
-    <!--  dialog confirm-->
-<!--    <el-dialog v-model="centerDialogVisible" title="Warning" width="30%" center>-->
-<!--    <span-->
-<!--    >It should be noted that the content will not be aligned in center by-->
-<!--      default</span-->
-<!--    >-->
-<!--      <template #footer>-->
-<!--      <span class="dialog-footer">-->
-<!--        <el-button @click="centerDialogVisible = false">Cancel</el-button>-->
-<!--        <el-button type="primary" @click="centerDialogVisible = false"-->
-<!--        >Confirm</el-button-->
-<!--        >-->
-<!--      </span>-->
-<!--      </template>-->
-<!--    </el-dialog>-->
-
-
     <!-- dialog Form -->
     <el-dialog v-model="dialogFormVisible" title="Shipping address">
       <el-form :model="form">
--- frontend/src/views/layout/sideBar/index.vue
+++ frontend/src/views/layout/sideBar/index.vue
@@ -6,149 +6,214 @@
  * @Description: 左邊菜單欄
  * @FilePath: \vue3-vite-ssis\src\views\layout\sideBar\index1.vue
 -->
-
 <template>
-  <div class="sidebar-container" :class="{'collapse-width': isCollapse}">
-    <el-menu
-        :default-active="$route.path"
-        class="el-menu-vertical-demo"
-        background-color="#304156"
-        text-color="rgb(200 212 226)"
-        router
-        :collapse="isCollapse"
-    >
-
-      <el-menu-item index="/dashboard">
-        <i class="ri-palette-line"></i>
-        <template #title>仪表盘</template>
-      </el-menu-item>
-
-      <el-submenu index>
-        <template #title>
-          <i class="ri-file-text-line"></i>
-          <span>安全监控</span>
-        </template>
-        <el-menu-item index="/helloworld">
-          <i class="ri-secure-payment-line"></i>
-          <template #title>安全事项</template>
-        </el-menu-item>
-        <el-menu-item index="/security">
-          <i class="ri-coupon-5-line"></i>
-          <template #title>安全标准</template>
-        </el-menu-item>
-      </el-submenu>
-      <el-menu-item index="/stationsetting">
-        <i class="ri-pencil-line"></i>
-        <template #title>场站设置</template>
-      </el-menu-item>
-
-      <el-menu-item index="/vendorsetting">
-        <i class="ri-discuss-line"></i>
-        <template #title>供应商设置</template>
-      </el-menu-item>
-
-      <el-menu-item index="/securityaccident">
-        <i class="ri-briefcase-4-line"></i>
-        <template #title>安全事故</template>
-      </el-menu-item>
-
-      <el-menu-item index="/testaxios">
-        <i class="ri-cactus-fill"></i>
-        <template #title>Aciox测试</template>
-      </el-menu-item>
-      <el-menu-item index="/crossdomain">
-        <i class="ri-cactus-fill"></i>
-        <template #title>跨域请求</template>
-      </el-menu-item>
-      <el-menu-item index="/env">
-        <i class="ri-haze-fill"></i>
-        <template #title>环境变量</template>
-      </el-menu-item>
-
-      <el-menu-item index="/introduce">
-        <i class="ri-user-voice-line"></i>
-        <template #title>简介页面</template>
-      </el-menu-item>
-      <el-menu-item index="/goodlist">
-        <i class="ri-shopping-bag-2-fill"></i>
-        <template #title>商品维护</template>
-      </el-menu-item>
-      <el-menu-item index="/characterRecognition">
-        <i class="ri-shopping-bag-2-fill"></i>
-        <template #title>文字识别</template>
-      </el-menu-item>
-      <el-menu-item index="/imageUpload">
-        <i class="ri-shopping-bag-2-fill"></i>
-        <template #title>图片上传</template>
-      </el-menu-item>
-      <el-menu-item index="/pagination">
-        <i class="ri-shopping-bag-2-fill"></i>
-        <template #title>分页</template>
-      </el-menu-item>
-      <el-menu-item index="/multisearch">
-        <i class="ri-search-2-fill"></i>
-        <template #title>多条件查询</template>
-      </el-menu-item>
-      <el-menu-item index="/baseform">
-        <i class="ri-search-2-fill"></i>
-        <template #title>基本表单</template>
-      </el-menu-item>
-
-    </el-menu>
+  <div class="layout">
+    <el-container v-if="state.showMenu" class="container">
+      <el-aside class="aside">
+        <div class="head">
+          <div>
+            <img src="https://s.weituibao.com/1582958061265/mlogo.png" alt="logo">
+            <span>vue3 admin</span>
+          </div>
+        </div>
+        <div class="line" />
+        <el-menu
+          :default-openeds="state.defaultOpen"
+          background-color="#222832"
+          text-color="#fff"
+          :router="true"
+          :default-active='state.currentPath'
+        >
+          <el-submenu index="1">
+            <el-icon><location /></el-icon>
+            <template #title>
+              <span>aaaaaaa</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/introduce"><i class="el-icon-data-line" />系统介绍</el-menu-item>
+              <el-menu-item index="/dashboard"><i class="el-icon-odometer" />Dashboard</el-menu-item>
+              <el-menu-item index="/add"><i class="el-icon-plus" />添加商品</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template #title>
+              <span>首页配置bbbb</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/swiper"><i class="el-icon-picture" />轮播图配置</el-menu-item>
+              <el-menu-item index="/hot"><i class="el-icon-star-on" />热销商品配置</el-menu-item>
+              <el-menu-item index="/new"><i class="el-icon-sell" />新品上线配置</el-menu-item>
+              <el-menu-item index="/recommend"><i class="el-icon-thumb" />为你推荐配置</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template #title>
+              <span>模块管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/category"><i class="el-icon-menu" />分类管理</el-menu-item>
+              <el-menu-item index="/good"><i class="el-icon-s-goods" />商品管理</el-menu-item>
+              <el-menu-item index="/guest"><i class="el-icon-user-solid" />会员管理</el-menu-item>
+              <el-menu-item index="/order"><i class="el-icon-s-order" />订单管理</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template #title>
+              <span>系统管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/account"><i class="el-icon-lock" />修改密码</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container class="content">
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import {reactive, computed} from 'vue'
-import {useStore, mapGetters, mapActions} from 'vuex'
+import { onUnmounted, reactive } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
-  name: 'siderBar',
-  computed: {
-    ...mapGetters(['isCollapse'])
+  name: 'App',
+  components: {
   },
+  setup() {
+    const noMenu = ['/login']
+    const router = useRouter()
+    const state = reactive({
+      defaultOpen: ['1', '2', '3', '4'],
+      showMenu: true,
+      currentPath: '/dashboard',
+      count: {
+        number: 1
+      }
+    })
+    // 监听浏览器原生回退事件
+    if (window.history && window.history.pushState) {
+      history.pushState(null, null, document.URL);
+      window.addEventListener('popstate', () => {
+        if (!localGet('token')) {
+          state.showMenu = false
+        }
+      }, false);
+    }
+    const unwatch = router.beforeEach((to, from, next) => {
+      if (to.path == '/login') {
+        // 如果路径是 /login 则正常执行
+        next()
+      } else {
+        // 如果不是 /login，判断是否有 token
+        if (!localGet('token')) {
+          // 如果没有，则跳至登录页面
+          next({ path: '/login' })
+        } else {
+          // 否则继续执行
+          next()
+        }
+      }
+      state.showMenu = !noMenu.includes(to.path)
+      state.currentPath = to.path
+      document.title = pathMap[to.name]
+    })
+
+    onUnmounted(() => {
+      unwatch()
+    })
+
+    return {
+      state
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
-.collapse-width {
-  width: 64px !important;
-
-}
-
-.sidebar-container {
-  width: 210px;
-  height: 100%;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  transition: width 0.3s;
-  z-index: 1001;
-  overflow: hidden;
-
-  .el-menu-vertical:not(.el-menu--collapse) {
-    width: 210px;
+<style scoped>
+  .layout {
+    min-height: 100vh;
+    background-color: #ffffff;
   }
-
-  .el-menu {
-    border: none;
-    height: 100%;
-    width: 100% !important;
+  .container {
+    height: 100vh;
+  }
+  .aside {
+    width: 200px!important;
+    background-color: #222832;
+    overflow: hidden;
     overflow-y: auto;
+    -ms-overflow-style: none;
+    overflow: -moz-scrollbars-none;
   }
-}
+  .aside::-webkit-scrollbar {
+    display: none;
+  }
+  .head {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+  }
+  .head > div {
+    display: flex;
+    align-items: center;
+  }
 
-.el-menu-item, .el-submenu {
-  text-align: left;
-}
-
-.el-menu-item i, .el-submenu__title i {
-  color: inherit;
-  font-size: 16px;
-  margin-right: 5px;
-
-}
-
-
+  .head img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+  .head span {
+    font-size: 20px;
+    color: #ffffff;
+  }
+  .line {
+    border-top: 1px solid hsla(0,0%,100%,.05);
+    border-bottom: 1px solid rgba(0,0,0,.2);
+  }
+  .content {
+    display: flex;
+    flex-direction: column;
+    max-height: 100vh;
+    overflow: hidden;
+  }
+  .main {
+    height: calc(100vh - 100px);
+    overflow: auto;
+    padding: 10px;
+  }
+</style>
+<style>
+  body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .el-menu {
+    border-right: none!important;
+  }
+  .el-submenu {
+    border-top: 1px solid hsla(0, 0%, 100%, .05);
+    border-bottom: 1px solid rgba(0, 0, 0, .2);
+  }
+  .el-submenu:first-child {
+    border-top: none;
+  }
+  .el-submenu [class^="el-icon-"] {
+    vertical-align: -1px!important;
+  }
+  a {
+    color: #409eff;
+    text-decoration: none;
+  }
+  .el-pagination {
+    text-align: center;
+    margin-top: 20px;
+  }
+  .el-popper__arrow {
+    display: none;
+  }
 </style>
