1ea045a57e8cd74033ea26a1a9b15cc6145044a6
jinsonleung <1053438108@qq.com>
2022-02-14 17:50:16 +0800

    airport/index.vue

--- frontend/src/views/freightTools/airport/index.vue
+++ frontend/src/views/freightTools/airport/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="freightTools-Airport-container">
     <el-card shadow="hover" header="机场代码查询">
-      <el-input size="small" placeholder="输入机场代码/机场名称/国家(地区)/城市" style="max-width: 280px"> </el-input>
-      <el-button type="primary" @click="" size="small">查询</el-button>
+      <el-input v-model="searchText" size="small" placeholder="输入机场代码/机场名称/国家(地区)/城市" style="max-width: 280px"> </el-input>
+      <el-button type="primary" @click="onSearchAirport" size="small">查询</el-button>
       <el-table :data="tableData" style="width: 100%">
 				<el-table-column prop="a1" label="IATA"> </el-table-column>
 				<el-table-column prop="a2" label="ICAO"> </el-table-column>
@@ -16,12 +16,21 @@
 
 <script lang="ts">
 
+import {ref} from "vue";
+
 export default {
   name: 'freightToolsAirport',
   components: {},
   setup() {
+    const searchText = ref('');
+
+		const onSearchAirport = () => {
+      console.log('==airport search==', 'searching...')
+		};
 
     return {
+      searchText,
+      onSearchAirport,
     };
   },
 };
