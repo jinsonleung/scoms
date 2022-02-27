5a4864cf99ed96339f51e04f1ed2de3242cc3c0c
JinsonLeung <1053438108@qq.com>
2022-02-27 21:30:32 +0800

    update db

--- backend/db/continent_country_state_city_airport_airline -v1.2.1/continent.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (1, 'Asia', '亚洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (2, 'Europe', '欧洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (3, 'Africa', '非洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (4, 'Oceania', '大洋洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (5, 'Antarctica', '南极洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (6, 'North America', '北美洲', null);
-INSERT INTO continent (id, eng_name, chn_name, description) VALUES (7, 'South America', '南美洲', null);
